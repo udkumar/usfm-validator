7cf49fb317bba5d5186b8c2dfb0a8018337debf9
Sony Zach <sony.z@bridgeconn.com>
2016-12-08 17:16:09 +0530

    function for validate

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+var fs = require('fs');
+var path = require('path')
+
+var fileExtension = require('file-extension');
+
+var filePath = './Tests/3JN.usfm';
+
+fs.readFile( filePath, 'utf8', function(err, data) {  
+
+	console.log(filePath);
+	function validateUsfm(usfmInput){
+
+	var validateUsfmFileExtension =  [".sfm", ".usfm"];   
+	fileExtension();
+
+	}
 
 
-console.log("test");
+    if (err) {
+    	console.log(err);
+    }
+    // console.log(filepath);
+});
+
+
