2c114e891754e8d3a56c47b370b914ca1ad33d3e
Sony Zach <sony.z@bridgeconn.com>
2016-12-14 17:26:55 +0530

    validate chapter data

--- index.js
+++ index.js
@@ -3,23 +3,70 @@ var path = require('path')
 
 var fileExtension = require('file-extension');
 
+var usfm = require('usfm-parser');
+
 var filePath = './Tests/3JN.usfm';
 
-fs.readFile( filePath, 'utf8', function(err, data) {  
+fs.readFile( filePath, 'utf8', function(err, data) {
 
-	console.log(filePath);
-	function validateUsfm(usfmInput){
+	var ext = fileExtension(filePath);
 
-	var validateUsfmFileExtension =  [".sfm", ".usfm"];   
-	fileExtension();
+	var toJSON = usfm.toJSON(data);
+	var idTag = toJSON.headers['id'];
 
-	}
-
+	if( ext == 'usfm' || ext == 'sfm'){	
+	    
+		var regEx = /(\d[A-Z]{2})|(\w[A-Z]{2})/;
+	    // the three letter code in the \id field must be capitalized
+	    if (idTag.match(regEx)){
+	    	validateChapter();
+	    }
+	    else{
+	    	console.log('\id tag is not fulfilled according to the usfm requirement');
+	    }
+    }
+	else {
+        console.log(' Only usfm file is allowed');
+    }
 
     if (err) {
     	console.log(err);
     }
-    // console.log(filepath);
+   	
+   	function validateChapter() {
+
+		var bookData = toJSON.chapters;
+
+		var chapterNum = [];
+		chapterNum = bookData[0].number;
+		// console.log(chapterNum);
+		var verses = bookData[0].verses;
+		console.log(verses.length)
+
+		var verseNum = [];
+		for(var i = 0; i < verses.length; i++) {
+          verseNum.push(verses[i].number);
+      	}
+      	console.log(verseNum);
+
+      	var duplicateVerseNum = [];
+      	for (var i = 0; i < verseNum.length - 1; i++) {
+      		if (verseNum[i + 1] == verseNum[i]) {
+        		duplicateVerseNum.push(verseNum[i]);
+    		}
+      	}
+      	console.log(duplicateVerseNum);
+
+      	var outs= [], L= verseNum.length, i= 0, prev;
+		while(i<L){
+    		prev= verseNum[i]; 
+    		while(verseNum[++i]<prev) outs.push(i);
+		}
+		console.log(outs);
+      	
+	}
+
 });
+
 
 
