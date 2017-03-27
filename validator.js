8c3a6b7c7c0dbcd2901bbb67e3c42a9af7096ed2
Sony Zach <sony.z@bridgeconn.com>
2017-03-27 14:32:51 +0530

    validation check for markers

--- validator.js
+++ validator.js
@@ -46,8 +46,8 @@ var s = fs.createReadStream('./Tests/3JN.usfm')
                 var obj = {};
                 for (var j=0; j<childArr.length; j++) {
                     var inlineArr = childArr[j].split(" ");
+                    
                     // object for inline markers
-                   
                     inlineMarker = inlineArr[0];
                     inlineValue = childArr[j].substr(childArr[j].indexOf(' ')+1) ;
                     // null values if only marker exist
@@ -79,8 +79,6 @@ var s = fs.createReadStream('./Tests/3JN.usfm')
             https://git.door43.org/Door43/ContentTechs/wiki/Minimum-USFM-Requirementss
         */
 
-        var chapNum = [];
-        var verseNum = [];
         for (var i in lines) {
 
             // marker \\id check
@@ -94,16 +92,16 @@ var s = fs.createReadStream('./Tests/3JN.usfm')
                     count = lines[i].count;
                     var regEx = /(\d[A-Z]{2})|(\w[A-Z]{2})/;
                     if (markerID.match(regEx)){
-                        console.log("The \\id marker of this usfm file is "+"'"+ markerID +"'"+" found in line "+ count)
+                        console.log("Info: The \\id marker is "+"'"+ markerID +"'"+" found in line "+ count)
                     }
                     else{
-                        console.log("The \\id marker is not validated acc to minimum usfm requirement--->"+"'"+markerID+"'");
+                        console.log("Warning: The \\id marker is not validated according to minimum usfm requirement--->"+"'"+markerID+"'");
                         console.log("The CODE is a standard 3 letter scripture book abbreviation found here:")
                         console.log("http://ubsicap.github.io/usfm/identification/books.html");
                     }
                 }
                 else{
-                    console.log("The initial USFM marker in any scripture text file is \\id and in this usfm file it is not found.")
+                    console.log("Error: The initial USFM marker in any scripture text file is \\id and it is not found in line 1")
                 }
             }
 
@@ -111,52 +109,75 @@ var s = fs.createReadStream('./Tests/3JN.usfm')
             if(lines[i].marker === 'ide'){
                 var markerIde = lines[i].value;
                 count = lines[i].count;
-                var regEx = /(UTF-8 || UTF-16 || CP-1252 || CP-1251 )/;
+                var regEx = /(UTF-8|UTF-16|CP-1252|CP-1251)/;
                 if(markerIde.match(regEx)){
-                    console.log("The \\ide marker of this file is "+"'"+ markerIde +"'"+" found in line "+ count);
+                    console.log("Info: The \\ide marker is "+"'"+ markerIde +"'"+" found in line "+ count);
+                }
+                else{
+                  console.log("Warning: The \\ide marker should contain a character encoding specification in line "+ count)
                 }
             }
 
-       //      if(v.marker === 'id'){
-       //      if (increment == 0){
-       //         var markerID = v.value.split(" ")[0];
-       //         count = v.count;
-       //         var regEx = /(\d[A-Z]{2})|(\w[A-Z]{2})/;
-       //         increment++;
-       //         if (markerID.match(regEx)){
-       //           console.log("The \\id marker of this usfm file is "+"'"+ markerID +"'"+" found in line "+ count)
-       //         }
-       //         else{
-       //            console.log("ID marker is not validated acc to minimum usfm requirement--->"+"'"+markerID+"'")
-       //         }
-       //      }else{
-       //          console.log("Check error")
-       //      }
-       //   }
-       //   if(v.marker === 'ide'){
-       //      if (increment == 0){
-       //         var markerIde = lines[i].value;
-       //         count = v.count;
-       //         increment++;
-       //          var regEx = /(UTF-8 || UTF-16 || CP-1252 || CP-1251 )/;
-       //          if(markerIde.match(regEx)){
-       //            console.log("The \\ide marker of this file is "+"'"+ markerIde +"'"+" found in line "+ count);
-       //          }
-       //          else{
-       //            console.log("Ide marker is not validated acc to minimum usfm requirement--->"+"'"+markerIde+"'")
-       //          }
-       //      }else{
-       //          console.log("Check error")
-       //      }
-       //   }
-       // })
-       
-            // else{
-            //         console.log("Ide marker is not validated acc to minimum usfm requirement--->"+"'"+markerIde+"'")
-            // }
+            //marker \\h check
+            if(lines[i].marker === 'h'){
+                var markerHeader = lines[i].value;
+                count = lines[i].count;
+                console.log("Info: The \\h marker is "+"'"+ markerHeader +"'"+" found in line "+ count);
+            }
+
+            //marker \\toc1 check
+            if(lines[i].marker === 'toc1'){
+                var markerToc1 = lines[i].value;
+                count = lines[i].count;
+                console.log("Info: The \\toc1 marker is "+"'"+ markerToc1 +"'"+" found in line "+ count);
+            }
+
+            //marker \\toc2 check
+            if(lines[i].marker === 'toc2'){
+                var markerToc2 = lines[i].value;
+                count = lines[i].count;
+                console.log("Info: The \\toc2 marker is "+"'"+ markerToc2 +"'"+" found in line "+ count);
+            }
+
+            //marker \\toc3 check
+            if(lines[i].marker === 'toc3'){
+                var markerToc3 = lines[i].value;
+                count = lines[i].count;
+                console.log("Info: The \\toc3 marker is "+"'"+ markerToc3 +"'"+" found in line "+ count);
+            }
+
+            //marker \\mt check
+            if(lines[i].marker === 'mt'){
+                var markerMt = lines[i].value;
+                count = lines[i].count;
+                console.log("Info: The \\mt marker is "+"'"+ markerMt +"'"+" found in line "+ count);
+            }
+
+            //marker \\c check
+            if(lines[i].marker === 'c'){
+                var markerChapter = lines[i].value;
+                console.log("This is Chapter "+markerChapter);
+                count = lines[i].count;
+                console.log("Info: The \\c marker for chapter "+"'"+ markerChapter +"'"+" is found in line "+ count);
+                // chapNum.push(markerChapter);
+            }
+
+            //marker \\p check
+            if(lines[i].marker === 'p'){
+                var markerPara = lines[i].value;
+                count = lines[i].count;
+                console.log("Info: The \\p marker for this chapter is found in "+ count);
+            }
+
+            //marker \\v check
+            if(lines[i].marker === 'v'){
+                var markerVerse = lines[i].number;
+                count = lines[i].count;
+                console.log("Info: The \\v marker for verse "+"'"+ markerVerse +"'"+" is found in line "+ count);
+                // verseNum.push(markerVerse);
+            }
 
         }
-
         console.log('Read entire file.')
     })
 );
